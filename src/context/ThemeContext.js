176a605e605c8555e32480a78ee4f4d25076c958
zapds <71247896+zapds@users.noreply.github.com>
2025-08-11 23:44:12 +0530

    add stuff

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,3 +1,5 @@
+"use client"
+
 import React, { createContext, useState, useContext, useMemo, useEffect } from 'react';
 import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
